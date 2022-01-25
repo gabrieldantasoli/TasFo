92d94d3f99aac1dccf758d532235491235ba26c9
Gabriel Dantas de Oliveira <gabriel36dantas@gmail.com>
2022-01-25 17:33:27 -0300

    Blogs section

--- interations/script.js
+++ interations/script.js
@@ -72,3 +72,26 @@ var swiper = new Swiper(".menu-slider", {
     clickable: true,
   },
 });
+
+var swiper = new Swiper(".blogs-slider", {
+  grabCursor: true,
+  loop: true,
+  centeredSlides: true,
+  autoHeight: true,
+  spaceBetween:20,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      700: {
+        slidesPerView: 2,
+      },
+      1000: {
+        slidesPerView: 3,
+      },
+  },
+});
