96f32e3d0582674c0f6f6b3c6070f08bc9469105
Gabriel Dantas de Oliveira <gabriel36dantas@gmail.com>
2022-01-25 11:05:31 -0300

    Popular dishes

--- interations/script.js
+++ interations/script.js
@@ -37,3 +37,26 @@ var swiper = new Swiper(".home-slider", {
         },
     },
   });
+
+let previewContainer = document.querySelector('.food-preview-container');
+let previewBOX = previewContainer.querySelectorAll('.food-preview');
+
+document.querySelectorAll('.food .slide').forEach(food => {
+  food.onclick= () => {
+    previewContainer.style.display = 'flex';
+    let name = food.getAttribute('data-name');
+    previewBOX.forEach(preview => {
+      let target = preview.getAttribute('data-target');
+      if (name == target) {
+        preview.classList.add('active');
+      };
+    });
+  };
+});
+
+previewContainer.querySelector('#close-preview').onclick = () => {
+  previewContainer.style.display = 'none';
+  previewBOX.forEach(close => {
+    close.classList.remove('active')
+  })
+}
